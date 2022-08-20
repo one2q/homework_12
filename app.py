3f4e3a0da61e8bfc8c94b3d632ba58e8b084711b
Valentin <val8673@gmail.com>
2022-08-20 18:44:38 +0500

    Almost ready

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
-
-POST_PATH = "posts.json"
-UPLOAD_FOLDER = "uploads/images"
+from main.views import main_blueprint
+from loader.views import loader_blueprint
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def page_index():
-    pass
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
+
+# @app.route("/list")
+# def page_tag():
+#     pass
 
 
-@app.route("/list")
-def page_tag():
-    pass
+# @app.route("/post", methods=["GET", "POST"])
+# def page_post_form():
+#     pass
+#
+#
+# @app.route("/post", methods=["POST"])
+# def page_post_upload():
+#     pass
+
+#
+# @app.route("/uploads/<path:path>")
+# def static_dir(path):
+#     return send_from_directory("uploads", path)
 
 
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
-
-
-@app.route("/uploads/<path:path>")
-def static_dir(path):
-    return send_from_directory("uploads", path)
-
-
-app.run()
+if __name__ == "__main__":
+    app.run()
 
--- functions.py
+++ functions.py
@@ -1,1 +1,43 @@
 import json
+
+ALLOWED_EXTENSIONS = {'png', 'jpeg'}
+UPLOAD_FOLDER = "uploads/images"
+
+
+def load_from_json(filename: str) -> list:
+	with open(filename, 'r', encoding='utf-8') as file:
+		data = json.load(file)
+		return data
+
+
+def get_user_request_post(data: list, user_request: str) -> list | bool:
+	result = []
+	for i in data:
+		if user_request.lower() in i['content'].lower():
+			result.append(i)
+	if result:
+		return result
+	return False
+
+
+def allowed_file(filename):
+	return '.' in filename and \
+	       filename.split('.', 1)[-1] in ALLOWED_EXTENSIONS
+
+
+def picture_save(picture) -> str:
+	filename = picture.filename
+	path = f"{UPLOAD_FOLDER}/{filename}"
+	picture.save(path)
+	return path
+
+
+def add_post(pic: str, text: str):
+	new_post = {'pic': '/'+pic, 'content': text}
+	data = load_from_json('posts.json')
+	data.append(new_post)
+	with open('posts.json', 'w', encoding='utf-8') as file:
+		json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+# add_post('1231', 'sdgsdgag')
