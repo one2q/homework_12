d562a0b216d78f465d6f043f78d86a4627ce4187
Valentin <val8673@gmail.com>
2022-08-23 13:16:52 +0500

    Перенес загрузку из json в блюпринт что бы можно было сразу найти пост

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request
 from functions import load_from_json, get_user_request_post
 
 POST_PATH = "posts.json"
-data = load_from_json(POST_PATH)
 
 # Создаем новый блюпринт
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
@@ -18,5 +17,5 @@ def page_search():
 	user_request = request.args['s']  # Запрос пользователя
 	if not user_request:  # Проверяем ввел ли пользователь строку и если нет ничего не происходит
 		return render_template('index.html')
-	post_list = get_user_request_post(data, user_request)
+	post_list = get_user_request_post(load_from_json(POST_PATH), user_request)
 	return render_template('post_list.html', user_request=user_request, post_list=post_list)
